d1c55ca0081b5624eaabf2c5d5409f4b4158e5d8
Mona Lisa <tweetwee1996@gmail.com>
2023-04-03 18:08:55 -0700

    run dist

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,7 +16,7 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.log('Put to the database');
   const jateDb = await openDB('jate', 1);
-  const tx = db.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.put({id: 1, value: content})
   const result = await request;
