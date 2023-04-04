2db05530160d3cb608ac74f83f792848eb170f82
Mona Lisa <tweetwee1996@gmail.com>
2023-04-03 22:07:18 -0700

    updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -39,8 +39,8 @@ module.exports = () => {
           description: 'text editor to work ',
           background_color: '#225ca3',
           theme_color: '#225ca3',
-          start_url: '/',
-          publicPath: '/',
+          start_url: './',
+          publicPath: './',
           icons: [
             {
               src: path.resolve('src/images/logo.png'),
